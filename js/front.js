1507d22f192648ce4de8fcc5f15c4a8d15b251c2
Timothée Crozat <timot+git@crachecode.net>
2017-02-18 02:02:20 +0000

    version 2 tissu gris bleu

--- js/front.js
+++ js/front.js
@@ -213,4 +213,8 @@ $(document).ready(function () {
 
 });
 
-
+rot13 = function(str){
+	return str.replace(/[a-zA-Z]/g,function(c){
+		return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);
+	});
+}
